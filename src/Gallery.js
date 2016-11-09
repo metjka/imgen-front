4514ab1ebaf04d194f82c7567b824e9d9d75281b
Ihor <ihor.salnikov@gmail.com>
2016-11-09 15:05:31 +0100

    design changes

---
+++ src/Gallery.js
@@ -0,0 +1,111 @@
+import React, {Component, PropTypes} from 'react';
+import {StyleSheet, css} from 'aphrodite';
+
+class Gallery extends Component {
+    constructor() {
+        super();
+
+        this.state = {
+            lightboxIsOpen: false,
+            currentImage: 0,
+        };
+    }
+
+    renderGallery() {
+        const {images} = this.props;
+
+        if (!images) return;
+
+        const gallery = images.map((img, i) => {
+            return (
+                <a
+                    href={img.src}
+                    className={css(classes.thumbnail, classes[img.orientation])}
+                >
+                    <img src={img.thumbnail} className={css(classes.source)}/>
+                </a>
+            );
+        });
+
+        return (
+            <div className={css(classes.gallery)}>
+                {gallery}
+            </div>
+        );
+    }
+
+    render() {
+        return (
+            <div className="section">
+                {this.props.heading && <h2>{this.props.heading}</h2>}
+                {this.props.subheading && <p>{this.props.subheading}</p>}
+                {this.renderGallery()}
+
+            </div>
+        );
+    }
+}
+;
+
+Gallery.displayName = 'Gallery';
+Gallery.propTypes = {
+    heading: PropTypes.string,
+    images: PropTypes.array,
+    showThumbnails: PropTypes.bool,
+    subheading: PropTypes.string,
+};
+
+const gutter = {
+    small: 2,
+    large: 4,
+};
+const classes = StyleSheet.create({
+    gallery: {
+        marginRight: -gutter.small,
+        overflow: 'hidden',
+
+        '@media (min-width: 500px)': {
+            marginRight: -gutter.large,
+        },
+    },
+
+    // anchor
+    thumbnail: {
+        boxSizing: 'border-box',
+        display: 'block',
+        float: 'left',
+        lineHeight: 0,
+        paddingRight: gutter.small,
+        paddingBottom: gutter.small,
+        overflow: 'hidden',
+
+        '@media (min-width: 500px)': {
+            paddingRight: gutter.large,
+            paddingBottom: gutter.large,
+        },
+    },
+
+    // orientation
+    landscape: {
+        width: '30%',
+    },
+    square: {
+        paddingBottom: 0,
+        width: '40%',
+
+        '@media (min-width: 500px)': {
+            paddingBottom: 0,
+        },
+    },
+
+    // actual <img />
+    source: {
+        border: 0,
+        display: 'block',
+        height: 'auto',
+        maxWidth: '100%',
+        width: 'auto',
+    },
+});
+
+export default Gallery;
--- src/UserForm.js
+++ src/UserForm.js
@@ -1,7 +1,33 @@
-import React from 'react'
-import NavBar from "./NavBar"
-import * as C from './C'
+import React from 'react';
+
+import NavBar from "./NavBar";
+import * as C from './C';
+import Gallery from "./Gallery"
+
+const DEFAULT_IMAGES = [
+    {id: '1470619549108-b85c56fe5be8', caption: 'Photo by Alan Emery', orientation: 'square', useForDemo: true}, // https://unsplash.com/photos/SYzUF6XcWBY (Flamingo)
+    {id: '1471079502516-250c19af6928', caption: 'Photo by Jeremy Bishop', orientation: 'landscape', useForDemo: true}, // https://unsplash.com/photos/GIpGxe2_cT4 (Turtle)
+    {id: '1454023492550-5696f8ff10e1', caption: 'Photo by Jessica Weiller', orientation: 'landscape', useForDemo: true}, // https://unsplash.com/photos/LmVSKeDy6EA (Tiger)
+    {id: '1470854989922-5be2f7456d78', caption: 'Photo by Piotr Łaskawski', orientation: 'landscape', useForDemo: true}, // https://unsplash.com/photos/GXMr7BadXQo (Hedgehog)
+    {id: '1470317596697-cbdeda56f999', caption: 'Photo by Michel Bosma', orientation: 'landscape', useForDemo: true}, // https://unsplash.com/photos/XgF9e93Tkt0 (Ladybug)
+];
+
+function makeUnsplashSrc(id) {
+    return `https://images.unsplash.com/photo-${id}?dpr=2&auto=format&w=1024&h=1024`;
+}
+function makeUnsplashSrcSet(id, size) {
+    return `https://images.unsplash.com/photo-${id}?dpr=2&auto=format&w=${size} ${size}w`;
+}
+function makeUnsplashThumbnail(id, orientation = 'landscape') {
+    const dimensions = orientation === 'square'
+        ? 'w=300&h=300'
+        : 'w=240&h=159';
+
+    return `https://images.unsplash.com/photo-${id}?dpr=2&auto=format&crop=faces&fit=crop&${dimensions}`;
+}
+
 class UserGalleryForm extends React.Component {
+
 
     constructor() {
         super();
@@ -10,36 +36,29 @@ class UserGalleryForm extends React.Component {
         };
     }
 
+    lol() {
+        console.log("lol")
+    }
+
     render() {
         return (
             <div>
                 <NavBar username={this.state.user['username']}/>
-                {/*<UserForm name={this.state.user['username']}/>*/}
+                <Gallery images={[
+                    {src: 'http://localhost:1113/v1/image/ihor_1478694773198',thumbnail:'http://localhost:1113/v1/image/ihor_1478694773198' ,showThumbnails:'true'},
+                    {src: 'http://localhost:1113/v1/image/ihor_1478694773198',thumbnail:'http://localhost:1113/v1/image/ihor_1478694773198' ,showThumbnails:'true'},
+                    {src: 'http://localhost:1113/v1/image/ihor_1478694773198',thumbnail:'http://localhost:1113/v1/image/ihor_1478694773198' ,showThumbnails:'true'},
+                    {src: 'http://localhost:1113/v1/image/ihor_1478694773198',thumbnail:'http://localhost:1113/v1/image/ihor_1478694773198' ,showThumbnails:'true'},
+                    {src: 'http://localhost:1113/v1/image/ihor_1478694773198',thumbnail:'http://localhost:1113/v1/image/ihor_1478694773198' ,showThumbnails:'true'},
+                    {src: 'http://localhost:1113/v1/image/ihor_1478694773198',thumbnail:'http://localhost:1113/v1/image/ihor_1478694773198' ,showThumbnails:'true'},
+                    {src: 'http://localhost:1113/v1/image/ihor_1478694773198',thumbnail:'http://localhost:1113/v1/image/ihor_1478694773198' ,showThumbnails:'true'},
+                    {src: 'http://localhost:1113/v1/image/ihor_1478694773198',thumbnail:'http://localhost:1113/v1/image/ihor_1478694773198' ,showThumbnails:'true'}
+                ]}/>
             </div>
         )
     }
 }
 
-class UserForm extends React.Component {
-    render() {
-        return (
-            <div>
-                <h1>{this.props.name}</h1>
-                <h1>{this.props.token}</h1>
-                <img
-                    id="avatar"
-                    src={C.IMAGE_STORE_DEV + '/v1/image/avatar?username=' + this.props.name }
-                    height="50"
-                    width="50"
-                />
-            </div>
-        );
-    };
 
-}
-
-UserForm.propsTypes = {
-    name: React.PropTypes.string,
-};
 
 export default UserGalleryForm
